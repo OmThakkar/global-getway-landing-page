bf11dd206bc6adc91fc734272029c24b61245301
Om <omthakkar2021.comp@mmcoe.edu.in>
2025-10-20 21:30:43 +0530

    Fix: Landing Page Changes

--- src/components/general/CallToAction.jsx
+++ src/components/general/CallToAction.jsx
@@ -1,12 +1,19 @@
 import React from "react";
+import { Phone } from "lucide-react";
+import { Mail } from "lucide-react";
 
 const CallToAction = () => {
   return (
-    <div className="fixed  bottom-6 z-20 flex gap-2 lg:right-6 justify-center w-full lg:w-auto">
-      <a href="tel:+919112377799" className="button primary-button shrink-0 text-sm md:text-base">
+    <div className="fixed bottom-6 z-20 flex w-full justify-center gap-2 lg:right-6 lg:w-auto">
+      <a href="tel:+919112377799" className="button primary-button shrink-0">
+        <Phone className="h-4 w-4" />
         Call Us Now
       </a>
-      <a href="mailto:info@globalgetway.com" className="button primary-button shrink-0 text-sm md:text-base">
+      <a
+        href="mailto:info@globalgetway.com"
+        className="button primary-button shrink-0"
+      >
+        <Mail className="h-4 w-4" />
         Request Quote
       </a>
     </div>
